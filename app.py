b41d69e5ee5f7a92d60997090d4719103dec80da
Colquide <danniela_jimenez@hotmail.com>
2024-11-23 13:46:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ st.balloons()
 # Show title and description.
 st.title("💬 Colquide")
 st.write(
-   "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-   "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-   "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+   "Repositorio de historias y anécdotas✍🏻 "
+   "El amor no es consuelo, es luz.🤍 "
+   "Te leemos👀"
 )
 openai_api_key = st.secrets["api_key"] 
 # Create an OpenAI client.
